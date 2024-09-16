c8b500e8fc0648264e8c27f151691c8a0e2f7fcb
4LEX <106479376+F0R5371@users.noreply.github.com>
2024-09-15 20:35:59 -0500

    INVENTORY SYSTEM PROTOTYPE

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    [Header("Item Properties")]
+    public int ID;
+    public Vector3 holdPosition;
+    public Sprite itemIcon;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Pick Up Item.cs
+++ Assets/Scripts/Pick Up Item.cs
@@ -3,33 +3,46 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class PickUpItem : MonoBehaviour
 {
     // Public:
 
     // Private:
+    // Is looking at item vars
     private Camera cam;
-    // variables as always
     private LayerMask itemMask;
-    
-    public SpawnItemsRandomly sir;
+    // Inventory UI
+    private int itemCount = 0;
+    private int slotSelected = 1;
+
+    private Transform canvas;
+    private Transform slots;
+
+    private Transform heldItemsContainer;
 
     void Start()
     {
         cam = transform.Find("Main Camera").GetComponent<Camera>();
-
         itemMask = LayerMask.GetMask("Item");
+
+        canvas = GameObject.Find("Canvas").transform;
+        slots = canvas.Find("Inventory").GetChild(0).transform;
+
+        heldItemsContainer = transform.Find("HeldItems");
     }
 
     void Update()
     {
         IsLookingAtItem();
+        DropItem();
 
-        if (sir.numObjects == sir.numOfObjectsLeft)
-        {
-            SceneManager.LoadScene(2);
-        }
+        UpdateSlot();
+        SelectSlot();
+
+        print($"slot: {slotSelected}");
+        print($"item count: {itemCount}");
     }
 
     private void IsLookingAtItem()
@@ -47,16 +60,80 @@ public class PickUpItem : MonoBehaviour
                 // if the key E is pressed it will "pick up" the item
                 if (Input.GetKeyDown(KeyCode.E))
                 {
-                    hit.collider.gameObject.SetActive(false);
-                    PickUp();
+                    PickUp(hit.collider.gameObject.transform);
                 }
             }
         }
     }
 
     // update the counter
-    private void PickUp()
+    private void PickUp(Transform item)
     {
+        if (itemCount == 5)
+            return;
 
+        itemCount++;
+
+        item.parent = heldItemsContainer;
+        item.localPosition = item.GetComponent<Item>().holdPosition;
+
+        slots.GetChild(slotSelected - 1).GetChild(1).GetComponent<Image>().sprite = item.GetComponent<Item>().itemIcon;
+        slotSelected++;
+    }
+
+    private void DropItem()
+    {
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            if (itemCount != 0)
+            {
+                heldItemsContainer.GetChild(slotSelected - 1).position = transform.position;
+                heldItemsContainer.GetChild(slotSelected - 1).parent = null;
+
+                slots.GetChild(slotSelected - 1).GetChild(1).GetComponent<Image>().sprite = null;
+                slotSelected--;
+
+                itemCount--;
+            }
+        }
+    }
+
+    private void SelectSlot()
+    {
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            slotSelected = 1;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            slotSelected = 2;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            slotSelected = 3;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            slotSelected = 4;
+        }
+        else if (Input.GetKeyDown(KeyCode.Alpha5))
+        {
+            slotSelected = 5;
+        }
+    }
+
+    private void UpdateSlot()
+    {
+        for (int i = 1; i < 6; i++)
+        {
+            if (i == slotSelected)
+            {
+                slots.GetChild(i - 1).GetChild(0).GetComponent<Image>().color = Color.red;
+            }
+            else
+            {
+                slots.GetChild(i - 1).GetChild(0).GetComponent<Image>().color = Color.white;
+            }
+        }
     }
 }
